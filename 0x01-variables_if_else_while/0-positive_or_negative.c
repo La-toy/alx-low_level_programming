539cfa6beb05f2eed4826d7296b88079033dd4e4
Latoya <dametra@gmail.com>
2022-09-09 07:56:38 -0700

    Complete the source code in order to print whet
    her the number stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/**
- *main - Entry point
- *
+/* This program will assign a random number to the variable n each time it is executed
  *Return: Always 0 (Success)
  */
 int main(void)
