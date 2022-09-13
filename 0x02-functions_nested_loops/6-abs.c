e9f01cd24cf2177db44efa01d1327129f88f058d
Latoya <dametra@gmail.com>
2022-09-13 13:53:05 -0700

    A function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,18 +5,13 @@
  * @n: number being tested
  *
  * Return: Always 0
- *
  */
 int _abs(int)
 {
 
 	if (n >= 0)
 	{
-		return (-n);
-	}
-	else
-	{
 		return (n);
 	}
-	return (0);
+		return (-n);
 }
