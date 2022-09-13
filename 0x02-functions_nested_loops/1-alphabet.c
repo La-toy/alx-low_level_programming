7f7d9025aabe703ace68dabd941cffbe623b6664
Latoya <dametra@gmail.com>
2022-09-13 11:56:09 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
