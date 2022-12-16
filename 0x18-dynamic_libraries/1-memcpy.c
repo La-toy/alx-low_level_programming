77f00bc02edfa6253365e4757009d9a75f5a0b08
Latoya <dametra@gmail.com>
2022-12-16 08:54:22 -0800

    A script that creates a dynamic library

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes from memory area @src
+ * to memory area @dest.
+ * @dest: Memory area to copy @src into..
+ * @src: Memory area to copy from.
+ * @n: The number of bytes to copy from memory area @src.
+ *
+ * Return: Pointer to @dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
